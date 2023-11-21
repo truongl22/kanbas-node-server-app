86cedfc459626cc3bad0074f04898fb0ade87cdc
Lam Truong <21truongl@gmail.com>
2023-11-21 03:01:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
