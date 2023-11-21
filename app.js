6dbb09cc05336f03e56dc215b9d41d625d69a8ea
Lam Truong <21truongl@gmail.com>
2023-11-21 11:46:58 -0500

    fixed bugs in routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
