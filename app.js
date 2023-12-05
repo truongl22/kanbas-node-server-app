338e569638da7e2ee5da1e6b9d7197973d93b6e9
Lam Truong <21truongl@gmail.com>
2023-12-04 22:14:39 -0500

    a6 added mongo

--- app.js
+++ app.js
@@ -5,10 +5,50 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+
+const CONNECTION_STRING = "mongodb+srv://giuseppi:supersecretpassword@cluster0.msw7tt7.mongodb.net/kanbas?retryWrites=true&w=majority" || "|| 'mongodb://127.0.0.1:27017/kanbas"
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+//mongodb://localhost:27017/
+//
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    // origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL
+  })
+);
+// const sessionOptions = {
+//   secret: "any string",
+//   resave: false,
+//   saveUninitialized: false,
+// };
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(
+  session(sessionOptions)
+);
+
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
